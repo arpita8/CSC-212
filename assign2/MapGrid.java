6496f3af3aedb9f50a2fd1d5b62c45e8d9de073d
Zacharias Jace <zjarthurs@gmail.com>
2013-02-01 19:43:31 -0500

    Switched Height and Width

--- assign2/MapGrid.java
+++ assign2/MapGrid.java
@@ -1,6 +1,13 @@
 import java.awt.*;
 import java.lang.reflect.Field;
 
+/**
+ *  Makes a MapGrid to be stored.
+ *  
+ *  @author  Zach Arthurs and Maneka Puligandla 
+ *  @version Wednesday, Feburary, 6, 2013
+ */
+
 public class MapGrid {
     
     /**2-D array of colours*/
@@ -10,8 +17,8 @@ public class MapGrid {
    /**
     *  Takes in height and width, initializes 2-D array of that size, fills with black
     */
-    public MapGrid(int height, int width) {
-	mapColors =  new Color[height][width];
+    public MapGrid(int width, int height) {
+	mapColors =  new Color[width][height];
 	
 	for (int i = 0; i < mapColors.length; i++) {
 	    for (int j = 0; j < mapColors[i].length; j++) {
@@ -22,12 +29,12 @@ public class MapGrid {
     
     /**Accessor: returns height of map*/
     public int returnHeight() {
-	return mapColors.length;
+	return mapColors[0].length;
     }
     
     /**Accessor: returns width of map*/
     public int returnWidth() {
-	return mapColors[0].length;
+	return mapColors.length;
     }
     
     /**Accessor: returns contents of a given square*/
