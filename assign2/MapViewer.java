68ae559b018dca631da88d583dc498f3ef9242b1
Zacharias Jace <zjarthurs@gmail.com>
2013-02-01 19:43:48 -0500

    MapViewer Initial Commit

---
+++ assign2/MapViewer.java
@@ -0,0 +1,106 @@
+import java.awt.*;
+import java.lang.reflect.Field;
+
+/**
+ *  Allows the MapGrid to be viewed.
+ *  
+ *  @author  Zach Arthurs and Maneka Puligandla 
+ *  @version Wednesday, Feburary, 6, 2013
+ */
+
+public class MapViewer {  // CHANGE THE CLASS NAME TO SOMETHING APPROPRIATE
+    
+	/** Stores MapGrid to be displayed. */
+	public static MapGrid grid;
+	
+	/** How big the width of the viewer area will be */
+	public static int viewerWidth;
+	
+	/** How big the height of the viewer area will be */
+	public static int viewerHeight;
+	
+	/** How big the map will be */
+	private static int scale;
+	
+	/** Location of the upper left hand corner of the map. */
+	private static Point offset;
+	 
+	/** Constructs MapView with defualt location and scale. */
+	public MapViewer(MapGrid grid) {
+		offset.setLocation(0,0);
+		scale = 10;
+	} 
+	
+	/** Accessor for grid */
+	public MapGrid getGrid() {
+		return grid;
+	}
+	
+	/** Accessor for viewerWidth */
+	public int getViewerWidth() {
+		return viewerWidth;
+	}
+	
+	/** Accessor for viewerHeight */
+	public int getViewerHeight() {
+		return viewerHeight;
+	}
+	
+	/** Accessor for scale */
+	public int getScale() {
+		return scale;
+	}
+	
+	/** Accessor for offset */
+	public Point getOffset() {
+		return offset;
+	}
+	
+	/** Manipulator for viewerWidth */
+	public void changeViewerWidth(int newWidth) {
+		viewerWidth = newWidth;
+	}
+	
+	/** Manipulator for viewerHeight */
+	public void changeViewerHeight(int newHeight) {
+		viewerHeight = newHeight;
+	}
+	
+	/** Manipulator for scale */
+	public void changeScale(int newscale) {
+		scale = newscale;
+	}
+	
+	/** Manipulator for offset */
+	public void changeOffset(Point newoffset) {
+		offset.setLocation(newoffset);
+	}
+	
+	/** Returns size of the displayed area */
+	public Dimension getMininmunSize() {
+		Dimension d = new Dimension(viewerWidth, viewerHeight);
+		return d;
+	}
+	
+	/** Returns size of the displayed area */
+	public Dimension getPreferredSize() {
+		Dimension d = new Dimension(viewerWidth, viewerHeight);
+		return d;
+	}
+	
+	/**
+	 * Draws the map.
+	 *
+	 * @params scale, offset, grid
+	 *
+	 */
+	public void paintComponent() {
+	}
+	
+    /**
+     *  DESCRIBE THE MAIN METHOD HERE
+     */    
+    public static void main(String[] args) {
+	// YOUR PROGRAM WILL BEGIN EXECUTION HERE
+    }
+}
