fcf39f462075c980804a5f3f6c20e330f9d5cd70
Zacharias Jace <zjarthurs@gmail.com>
2013-03-28 16:52:36 -0400

    Boilerplate for Maze function

---
+++ assign7/Maze.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+
+/**
+ *  Takes a Maze and allows it to be viewed and manipulated
+ *  
+ *  @author Zacharias J. Arthurs
+ *  @version CSC 212, March 27, 2013
+ */
+
+public class Maze extends JComponent {  
+    // PUT CLASS FIELDS HERE (IF ANY), WITH JAVADOC FOR EACH
+
+    // PUT CONSTRUCTORS NEXT (IF ANY), WITH JAVADOC FOR EACH
+
+    // PUT CLASS METHODS NEXT (IF ANY), WITH JAVADOC FOR EACH
+}
+
