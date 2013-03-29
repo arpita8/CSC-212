98f68d40e9002b26044eaf7f705e7a6ff4ef5176
Zacharias Jace <zjarthurs@gmail.com>
2013-03-28 23:53:16 -0400

    doesnt work good enough

--- lab7/TestBinaryTree.java
+++ lab7/TestBinaryTree.java
@@ -10,11 +10,20 @@ public class TestBinaryTree {
      *  Tests Binary Tree
      */    
     public static void main( ) {
-	public BinaryTree<Double> t1;
-	public BinaryTree<Double> t2;
-	public BinaryTree<Double> t3;
-	public BinaryTree<Double> t4;
+	BinaryTree<Double> t1;
+	BinaryTree<Double> t2;
+	BinaryTree<Double> t3;
+	BinaryTree<Double> t4;
 	
-	t1 = new BinaryTree(double 4.0, BinaryTree<double> 3.5, BinaryTree<Double> 5.5);
-    }
+	t1.setData(4.0);
+	t1.setLeft(3.5, BinaryTree<Double> (1.25), BinaryTree<Double> (3.75));
+	t1.setRight(5.5, BinaryTree<Double> (4.75), BinaryTree<Double> (8.5, BinaryTree<Double> (7.0), BinaryTree<Double> (13.0)));
+	
+	t2.BinaryTree(t1);
+	t2.setData(Double -1);
+	System.out.println("t1");
+	t1.printIndented;
+	System.out.println("t2");
+	t2.printIndented;
+	}
 }
